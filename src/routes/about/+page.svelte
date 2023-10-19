73e037ac8962ef06f80c06a1b34c81ed874f0492
Don <don.qcastillo@yahoo.com>
2023-10-19 03:38:31 -0600

    updated how to play page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,8 @@
 <section class="container max-w-[1124px] p-5 mx-5 min-h-[500px]">
     <h2 class="text-white text-center mb-5 text-4xl color-selected-2 font-heading-regular">How to Play the Game?</h2>
     <div class="text-white font-normal-regular text-2xl">
-        Answer the questions as many as possible. Each correct answer has a corresponding prize. Once you reach the safe haven prizes, you are guaranteed to get those amount even if you answer incorrectly. You can quit anytime within the game.
+        Answer as many questions as possible. Each correct answer has a corresponding prize. If you answer incorrectly, you lose the game. Once you reach the safe haven prizes (prizes that are marked with diamonds), you are guaranteed to get those amount even if you answer incorrectly. You can quit anytime within the game.
+        <br>
+        Lifelines are not implemented.
     </div>
 </section>
